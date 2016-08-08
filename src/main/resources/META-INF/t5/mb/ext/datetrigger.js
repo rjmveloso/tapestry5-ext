03d3999ab47ec1fd8ecfeb4ce8c7134f8f61b270
rveloso <rveloso@rveloso.local>
2016-08-08 11:29:41 +0100

    New dateboundaries mixin that validates if an initial date is after a final date, and vice-versa.

--- src/main/resources/META-INF/t5/mb/ext/datetrigger.js
+++ src/main/resources/META-INF/t5/mb/ext/datetrigger.js
@@ -2,13 +2,23 @@
 	define([ 't5/core/datepicker', 't5/core/dom', 'moment', 'jquery' ], function(DatePicker, dom, moment, $) {
 		var datepicker = null, visible = false;
 		
+		// watch out => this makes use of internal jQuery structure
+		$.fn.bindFirst = function(name, fn) {
+			this.bind(name, fn);
+			
+			var event = name.split('.')[0];
+			for(i = 0; i < this.length; i++) {
+				var elem = this[i];
+				var handlers = $._data(elem).events[event];
+				handlers.unshift(handlers.pop());
+			}
+		};
+		
 		$(document).ready(function() {
 		    dom.scanner('[data-component-wrapper="datefield"]', function(container) {
 		        container.attr("data-component-wrapper", null);
-		        container.parent().findFirst('button').on('click', function() {
-					datepicker = container.$;
-				});
-		        container.parent().findFirst('.glyphicon').on('click', function() {
+		        var button = container.parent().findFirst('button');
+		        button.$.bindFirst('click', function() {
 		        	datepicker = container.$;
 		        });
 		    });
